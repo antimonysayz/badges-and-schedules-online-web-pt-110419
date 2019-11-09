2556c3b3da09a045abc227e38f6c0e3466749d2b
Anthony Mai <anthony.mai530@gmail.com>
2019-11-09 21:13:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,9 +10,3 @@ def batch_badge_creator(array)
   array.each{|name| badges << "Hello, my name is #{name}."}
   badges
 end
-
-def assign_rooms(array)
-  rooms = []
-  array.each_with_index{|i| rooms << "Hello, #{array[i]}! You'll be assigned to room #{i}!"}
-  rooms
-end
