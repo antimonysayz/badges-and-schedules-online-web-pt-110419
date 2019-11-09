3458c46cb20ebb1233f70bb99cba43b252089127
Anthony Mai <anthony.mai530@gmail.com>
2019-11-09 21:08:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,9 @@ def badge_maker(name)
   badge = "Hello, my name is #{name}."
   badge
 end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each{|name| badges << "Hello, my name is #{name}."
+  badges
+end
